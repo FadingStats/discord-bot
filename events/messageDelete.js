77d019d402ccdb6314bb86ded559c4b3e34b2132
Ben Handsley <benhands12@gmail.com>
2017-12-30 00:29:57 +0000

    Update messageDelete.js
    Update the file to show who deleted the message.

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -19,14 +19,13 @@ module.exports = oldMessage => {
         return;
     }
   }
-  oldMessage.guild.fetchAuditLogs().then(() => {
-    // const user = logs.entries.first().executor;
+  oldMessage.guild.fetchAuditLogs({type: 72 /* MESSAGE_DELETE */ }).then(logs => {
       const embed = new Discord.RichEmbed()
       .setAuthor(oldMessage.author.tag, oldMessage.author.displayAvatarURL)
       .setDescription(
         `**Message Sent by ${oldMessage.author} in ${
           oldMessage.channel
-        } was deleted!**\n\n**Message:**\n${oldMessage.content}\n`,
+        } was deleted by ${logs.entries.first().executor}!**\n\n**Message:**\n${oldMessage.content}\n`,
       )
       .setThumbnail(
         "http://www.free-icons-download.net/images/full-trash-can-icon-27619.png",
