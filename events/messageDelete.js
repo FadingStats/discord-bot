960376314975e7f775c0adb6a362f633c445c0cf
Ben Handsley <benhands12@gmail.com>
2017-12-30 00:27:19 +0000

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -14,8 +14,10 @@ module.exports = oldMessage => {
     if (client.commands.has(command) || client.aliases.has(command)) return;
   }
   
-  if(oldMessage.channel.id === '380340086543482882' & '385822064474324992' & '395684180341686272' & '385814467079831552' & '386181943726702594' & '385820799505924105' & '386130781413703690' & '385818195707166721' & '385816775498924033' & '385743542879911948' & '385816300347326476' & '386964328076804118' & '380555872105005058' & '385816995460939776'){
-    return;
+  for (i = 0; i < settings.ignoredChannels.length; i +=1) {
+    if (oldMessage.channel.id === settings.ignoredChannels[i]) {
+        return;
+    }
   }
   oldMessage.guild.fetchAuditLogs().then(() => {
     // const user = logs.entries.first().executor;
