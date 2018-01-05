50fe67270bf3da83ba0d184d049266fe38085604
Shane Wilson <shane@Synplexco.uk>
2018-01-04 16:10:52 -0800

    Update user avatar if null

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,19 +1,26 @@
 const Discord = require('discord.js');
 
 // Start of Member Add Event
-module.exports = member => {
-  console.log(`${member.user.username} has joined HGVMP!`);
-  
-  const role = member.guild.roles.find('name', 'Driver');
-  member.addRole(role);
-  
+module.exports = (member) => {
+  console.log(`${member.user.username} has joined the server!`);
+
   const welcomeChannel = member.guild.channels.find('name', 'welcome');
   const rulesChannel = member.guild.channels.find('name', 'rules');
   const embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setTitle("New Member!")
-  .setDescription(`Welcome ${member.user} to ${member.guild.name}! Be sure to read ${rulesChannel} and enjoy your stay!`)
-  .setThumbnail(member.user.avatarURL);
-  welcomeChannel.send({embed})
+    .setColor('RANDOM')
+    .setTitle('New Member!')
+    .setDescription(
+      `Welcome ${member.user} to ${
+        member.guild.name
+      }! Be sure to read ${rulesChannel} and enjoy your stay!`,
+    )
+    .setThumbnail(
+      member.user.avatarURL ||
+        'https://camo.githubusercontent.com/1d25b77d1fb7e3f24fe2ef0063effe1981cb3f9c/687474703a2f2f6973322e6d7a7374617469632e636f6d2f696d6167652f7468756d622f507572706c65332f76342f65332f39642f32332f65333964323339652d376237612d653362372d633762662d3163313630653937633238632f6d7a6c2e646e6563666b6e702e706e672f30783073732d38352e6a7067',
+    );
+  welcomeChannel.send({ embed });
+
+  // Tried to add reaction via bot, gave up. For now Glanshon can be the one to rain them with emoji :D
+  
 };
 // End of Member Add Event
