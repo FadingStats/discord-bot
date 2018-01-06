e7dd0a383250454136deb195d41f078a95b96e95
Ben Handsley <benhands12@gmail.com>
2018-01-06 03:05:27 +0000

    Create popcorn.js
    Seriously, don't ask Inclusive about this.

---
+++ commands/popcorn.js
@@ -0,0 +1,34 @@
+// Start of Constants
+const settings = require("../storage/settings.json");
+// End of Constants
+
+// Start of "Popcorn" Command
+exports.run = (client, message) => {
+  message.delete().catch(console.error);
+
+  if (message.channel.id !== settings.commandsChannel) {
+    const botRoom = message.guild.channels.find("id", settings.commandsChannel);
+    return message.channel.send(
+      `Whoops, it looks like you're not in the ${botRoom} channel`,
+    );
+  }
+  message.channel.send(message.author.toString() + " Your popcorn, sir/madam :popcorn: ");
+};
+// End of "Popcorn" Command
+
+// Start of Permission System, etc
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+};
+// End of Permission System, etc
+
+// Start of Misc.
+exports.help = {
+  name: "popcorn",
+  description: "Gives you popcorn. (Don't ask Inclusive, Synplex made him.)",
+  usage: "popcorn",
+};
+// End of Misc.
