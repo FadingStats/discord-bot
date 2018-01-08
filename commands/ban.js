1979ab096f225ca3f020168011af05b6ebf15f05
Ben Handsley <benhands12@gmail.com>
2018-01-08 22:17:20 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,7 +1,7 @@
 // Start of Constants
 const {RichEmbed} = require('discord.js');
-const {caseNumber} = require('../util/caseNumber.js');
-const {parseUser} = require('../util/parseUser.js');
+const {caseNumber} = require('../storage/caseNumber.js');
+const {parseUser} = require('../storage/parseUser.js');
 const settings = const settings = require("../storage/settings.json");
 // End of Constants
 
