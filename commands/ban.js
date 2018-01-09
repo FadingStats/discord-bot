871758666631eb4f95d4dd063ab7c2eb5a16af60
Ben Handsley <benhands12@gmail.com>
2018-01-09 03:24:30 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,4 @@
 // Start of Constants
-const {RichEmbed} = require('discord.js');
-const {caseNumber} = require('../storage/caseNumber.js');
 const {parseUser} = require('../storage/parseUser.js');
 const settings = require("../storage/settings.json");
 // End of Constants
@@ -8,32 +6,14 @@ const settings = require("../storage/settings.json");
 // Start of "Ban" Command
 exports.run = async (client, message, args) => {
   const user = message.mentions.users.first();
+  const reason = args.slice(1, args.length).join(" ");
   parseUser(message, user);
-  const modlog = client.channels.find("id", settings.moderationLogsChannel);
-  const caseNum = await caseNumber(client, modlog);
-  if (!modlog)
-    return message.reply("I cannot find the moderation-log channel!");
-  if (message.mentions.users.size < 1)
-  return message
-    .reply("You must mention someone to warn them.")
-    .catch(console.error);
-  const reason =
-  args.splice(1, args.length).join(" ") ||
-  `Awaiting moderator's input. Use ${settings.prefix}reason ${
-    caseNum
-  } <reason>`;
-  const embed = new RichEmbed()
-  .setColor(0x00ae86)
-  .setTimestamp()
-  .setDescription(
-    `**Action:** Ban\n**Target:** ${user.tag}\n**Moderator:** ${
-      message.author.tag
-    }\n**Reason:** ${reason}`,
-  )
-  .setFooter(`Case ${caseNum}`);
-return client.channels.get(modlog.id).send({ embed });
+  message.guild.member(user).ban({
+    reason: `${reason}`
+  });
+  message.channel.send(`The member ${user.username} has been banned with the reason ${reason}!`);
 };
-// End of "Warn" Command.
+// End of "Ban" Command.
   
   // Start of Permission Level Setting, etc.
 exports.conf = {
