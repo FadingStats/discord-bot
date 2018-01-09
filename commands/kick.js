3e125212492cae2b9b3d5040416a9c42903f4b10
Ben Handsley <benhands12@gmail.com>
2018-01-09 03:27:36 +0000

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,6 +1,4 @@
 // Start of Constants
-const {RichEmbed} = require('discord.js');
-const {caseNumber} = require('../storage/caseNumber.js');
 const {parseUser} = require('../storage/parseUser.js');
 const settings = require('../storage/settings.json');
 // End of Constants
@@ -8,23 +6,19 @@ const settings = require('../storage/settings.json');
 // Start of "Kick" Command
 exports.run = async (client, message, args) => {
   const user = message.mentions.users.first();
+  const reason = args.slice(1, args.length).join(" ");
   parseUser(message, user);
-  const modlog = client.channels.find("id", settings.moderationLogsChannel);
-  const caseNum = await caseNumber(client, modlog);
-  if(!modlog) return message.reply('I cannot find a moderation-logs channel');
-  if(message.mentions.users.size < 1) return message.reply('You must mention someone to kick them.').catch(console.error);
-  
-  const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
-  const embed = new RichEmbed()
-  .setColor(0x00AE86)
-  .setTimestamp()
-  .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
-  .setFooter(`Case ${caseNum}`);
-  return client.channels.get(modlog.id).send({embed});
+  message.guild.member(user).kick({
+    reason: `${reason}`
+  });
+  message.channel.send(`The member ${user.username} has been kicked with the reason ${reason}!`);
 };
+// End of "Kick" Command
 
 exports.conf = {
-  aliases: [],
+  enabled: true,
+  guildOnly: false,
+  aliases: ["k"],
   permLevel: 1
 };
 
