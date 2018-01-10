24f4930bfbcf4550a8e1ebae7314a8026f934a4a
Ben Handsley <benhands12@gmail.com>
2018-01-10 16:00:37 +0000

    Update messageUpdate.js
    Adding an check if the messages extend the character limit, which is 2048 characters.

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -12,24 +12,82 @@ module.exports = (oldMessage, newMessage) => {
     }
   }
     newMessage.guild.fetchAuditLogs().then(() => {
-      // const user = logs.entries.first().executor;
-      const embed = new Discord.RichEmbed()
-        .setAuthor(newMessage.author.tag, newMessage.author.displayAvatarURL)
-        .setDescription(
-          `**Message Sent by ${newMessage.author} in ${
-            newMessage.channel
-          } was edited!**\n\n**Old MSG:**\n${
-            oldMessage.content
-          }\n\n**New MSG:**\n${newMessage.content}`,
-        )
-        .setThumbnail(
-          "http://www.freeiconspng.com/uploads/pencil-web-pencil-png-pencil-icon-flat-icon-png-1.png",
-        )
-        .setColor("RANDOM")
-        .setTimestamp();
-      newMessage.guild.channels
-        .find("id", settings.moderationLogsChannel)
-        .send({ embed });
+		if(oldMessage.content.length > 2048){
+			oldMessage.content = 'The old message extends the character limit (1024)!';
+			const embed = new Discord.RichEmbed()
+			.setAuthor(newMessage.author.tag, newMessage.author.displayAvatarURL)
+			.setDescription(
+			  `**Message Sent by ${newMessage.author} in ${
+				newMessage.channel
+			  } was edited!**\n\n**Old MSG:**\n${
+				oldMessage.content
+			  }\n\n**New MSG:**\n${newMessage.content}`,
+			)
+			.setThumbnail(
+			  "http://www.freeiconspng.com/uploads/pencil-web-pencil-png-pencil-icon-flat-icon-png-1.png",
+			)
+			.setColor("RANDOM")
+			.setTimestamp();
+			newMessage.guild.channels
+			.find("id", settings.moderationLogsChannel)
+			.send({ embed });
+		} else {
+			const embed = new Discord.RichEmbed()
+			.setAuthor(newMessage.author.tag, newMessage.author.displayAvatarURL)
+			.setDescription(
+			  `**Message Sent by ${newMessage.author} in ${
+				newMessage.channel
+			  } was edited!**\n\n**Old MSG:**\n${
+				oldMessage.content
+			  }\n\n**New MSG:**\n${newMessage.content}`,
+			)
+			.setThumbnail(
+			  "http://www.freeiconspng.com/uploads/pencil-web-pencil-png-pencil-icon-flat-icon-png-1.png",
+			)
+			.setColor("RANDOM")
+			.setTimestamp();
+			newMessage.guild.channels
+			.find("id", settings.moderationLogsChannel)
+			.send({ embed });
+		}
+		if(newMessage.content.length > 2048){
+			newMessage.content = 'The new message extends the character limit (1024)!';
+			const embed = new Discord.RichEmbed()
+			.setAuthor(newMessage.author.tag, newMessage.author.displayAvatarURL)
+			.setDescription(
+			  `**Message Sent by ${newMessage.author} in ${
+				newMessage.channel
+			  } was edited!**\n\n**Old MSG:**\n${
+				oldMessage.content
+			  }\n\n**New MSG:**\n${newMessage.content}`,
+			)
+			.setThumbnail(
+			  "http://www.freeiconspng.com/uploads/pencil-web-pencil-png-pencil-icon-flat-icon-png-1.png",
+			)
+			.setColor("RANDOM")
+			.setTimestamp();
+			newMessage.guild.channels
+			.find("id", settings.moderationLogsChannel)
+			.send({ embed });
+		} else {
+			const embed = new Discord.RichEmbed()
+			.setAuthor(newMessage.author.tag, newMessage.author.displayAvatarURL)
+			.setDescription(
+			  `**Message Sent by ${newMessage.author} in ${
+				newMessage.channel
+			  } was edited!**\n\n**Old MSG:**\n${
+				oldMessage.content
+			  }\n\n**New MSG:**\n${newMessage.content}`,
+			)
+			.setThumbnail(
+			  "http://www.freeiconspng.com/uploads/pencil-web-pencil-png-pencil-icon-flat-icon-png-1.png",
+			)
+			.setColor("RANDOM")
+			.setTimestamp();
+			newMessage.guild.channels
+			.find("id", settings.moderationLogsChannel)
+			.send({ embed });
+		}
     });
   }
 };
