16c2be3c1ff5945eb2dc2b428f5da7ede1a5dcd3
Ben Handsley <benhands12@gmail.com>
2018-01-11 18:02:47 +0000

    Update guildMemberAdd.js
    Let the bot wave to the user as a reaction!

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -22,9 +22,9 @@ module.exports = (member) => {
       member.user.avatarURL ||
         'https://camo.githubusercontent.com/1d25b77d1fb7e3f24fe2ef0063effe1981cb3f9c/687474703a2f2f6973322e6d7a7374617469632e636f6d2f696d6167652f7468756d622f507572706c65332f76342f65332f39642f32332f65333964323339652d376237612d653362372d633762662d3163313630653937633238632f6d7a6c2e646e6563666b6e702e706e672f30783073732d38352e6a7067',
     );
-  welcomeChannel.send({ embed });
-
-  // Tried to add reaction via bot, gave up. For now Glanshon can be the one to rain them with emoji :D
+  welcomeChannel.send({ embed }).then(function (message) {
+    message.react("👋")
+  });
   
 };
 // End of Member Add Event
