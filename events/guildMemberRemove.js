75a5f3c79c92ece24fc150d320b8895cbb26bf4d
FadingStats <thekingseb64@gmail.com>
2018-01-12 17:54:58 +0000

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,5 @@
 // Start of Member Remove Event
 module.exports = member => {
-  console.log(`${member.user.username} has left HGVMP!`);
+  console.log(`${member.user.username} has left Quantum!`);
 };
 // End of Member Add Event
